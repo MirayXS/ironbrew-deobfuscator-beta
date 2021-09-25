74e1e92600e781d1f80b051fb64ea9e20f436fbe
Cyclops <88403645+cccyclops@users.noreply.github.com>
2021-09-25 14:32:49 +0300

    Update devirtualizer_main.hpp

--- ironbrew_devirtualizer/devirtualizer_main.hpp
+++ ironbrew_devirtualizer/devirtualizer_main.hpp
@@ -7,6 +7,8 @@
 #include "symbolic_execution/loop_unrolled_bst.hpp"
 #include "vm_arch/proto.hpp"
 
+#include <mutex>
+
 namespace deobf::ironbrew_devirtualizer {
 	struct ironbrew_devirtualizer final {
 		void devirtualize();
@@ -16,6 +18,8 @@ namespace deobf::ironbrew_devirtualizer {
 			context(context)
 		{ }
 	private:
+		static std::recursive_mutex devirtualizer_mutex;
+
 		ast::ir::statement::block* context; // deobfuscation context
 		std::unique_ptr<symbolic_execution::loop_unrolled_bst> search_tree;
 	};
