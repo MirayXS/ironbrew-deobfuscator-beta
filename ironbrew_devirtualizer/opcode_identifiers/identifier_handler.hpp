e4af6ca51dc56e3173f3528275b5ce000baced8b
Cyclops <88403645+cccyclops@users.noreply.github.com>
2021-09-27 22:46:46 +0300

    Update identifier_handler.hpp

--- ironbrew_devirtualizer/opcode_identifiers/identifier_handler.hpp
+++ ironbrew_devirtualizer/opcode_identifiers/identifier_handler.hpp
@@ -5,12 +5,12 @@
 
 #include "../../vm_arch/opcode.hpp"
 
-// the following implementation uses the chain-of-responsibility design pattern to handle virtual opcodes
+// the following implementation uses the chain-of-responsibility design pattern to handle virtual opcodes and priority might be changed at runtime when we update to make it depend on stuff
 
 namespace deobf::ironbrew_devirtualizer::opcode_identifiers {
 	using namespace ast;
 
-	// abstract handler interface
+	// basic handler interface
 	struct handler_interface {
 	public: // todo: operator= instead of set_next_handler?
 		virtual handler_interface* set_next_handler(std::unique_ptr<handler_interface>&& handler) = 0; // forward function
@@ -20,7 +20,7 @@ namespace deobf::ironbrew_devirtualizer::opcode_identifiers {
 	};
 
 
-	// basic handler
+	// abstract handlers
 	struct basic_opcode_handler : public handler_interface {
 		handler_interface* set_next_handler(std::unique_ptr<handler_interface>&& handler) override;
 
